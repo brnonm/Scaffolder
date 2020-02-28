17bfeb260582c3617b8df57ec68f813c175ab8ba
pedrocarvalho <prrc97@icloud.com>
2020-02-28 12:44:28 +0000

    1

--- resources/views/scaffolder/configuretables.blade.php
+++ resources/views/scaffolder/configuretables.blade.php
@@ -7,14 +7,30 @@
     </div>
     <div class="row">
 
-        <div class="col-md-4">
-            <select class="form-control">
+        <div class="col-md-12">
 
+            <div id="accordion">
                 @foreach($tables as $table)
 
-                    <option>{{$table->$db}}</option>
-                @endforeach
-            </select>
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#{{$table->$db}}" aria-expanded="true" aria-controls="{{$table->$db}}">
+                                        Configurar Tabela {{$table->$db}}
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="{{$table->$db}}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                <div class="card-body">
+                                    {{$table->$db}}
+                                </div>
+                            </div>
+                        </div>
+
+
+            @endforeach
+            </div>
         </div>
     </div>
     <br>
