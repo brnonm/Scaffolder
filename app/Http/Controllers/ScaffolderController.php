95476c17c93986e6a77aed89404e2e902072df91
Bruno Oliveira <brnonm@hotmail.com>
2020-03-02 15:48:25 +0000

    json

--- app/Http/Controllers/ScaffolderController.php
+++ app/Http/Controllers/ScaffolderController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use function foo\func;
+use Illuminate\Support\Facades\Storage;
 
 
 class ScaffolderController extends Controller
@@ -23,37 +21,44 @@ class ScaffolderController extends Controller
 
         $tables = DB::select(DB::raw("SELECT TABLE_NAME AS _table FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$request->db'"));
 
-        foreach ($tables as $key=>$table) {
+        foreach ($tables as $key => $table) {
             $t = $table->_table;
             $columns = DB::select(DB::raw("show fields from " . $t));
 
 
-            $atr=[];
+            $atr = [];
             foreach ($columns as $column) {
-                $f=$column->Field;
+                $f = $column->Field;
 
 
-                $atr[$f]=$column;
-
-
-
+                $atr[$f] = $column;
             }
-            $metadados[$t]=$atr;
-
-
-
-
-
+            $metadados[$t] = $atr;
         }
-
-
-
+        $this->generateJson($metadados);
 
         return view("scaffolder.configuretables", compact("metadados"));
     }
 
-    public function tablesConfigureP1Post(Request $request){
+    public function tablesConfigureP1Post(Request $request)
+    {
 
     }
+
+
+    public function generateJson($metadados){
+
+
+        $contents = json_encode($metadados);
+        Storage::delete("scaffolderConfigs.json");
+        Storage::disk('local')->put("scaffolderConfigs.json", $contents);
+
+
+        //ler
+        $readData = Storage::get('scaffolderConfigs.json');
+        $data = json_decode($readData);
+        //dd($data);
+
+}
 
 }
